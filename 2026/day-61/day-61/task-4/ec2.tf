27eb2cd931f10a4be69d6e26ddfb7299cc10bb81
krmaryum <krmaryum@yahoo.com>
2026-04-22 21:53:11 -0500

    ec2-update

--- 2026/day-61/day-61/task-4/ec2.tf
+++ 2026/day-61/day-61/task-4/ec2.tf
@@ -5,13 +5,15 @@ provider "aws" {
 
 # Key Pair
 resource "aws_key_pair" "my_key_pair" {
-  key_name   = "terra-automate-key-josh"
+  key_name   = "terra-automate-key"
   public_key = file("terra-automate-key.pub")
 }
 
 # Default VPC
 resource "aws_default_vpc" "default" {
 }
+
+# Terraform AWS Security Group and EC2 Configuration
 
 # Security Group
 resource "aws_security_group" "my_security_group" {
@@ -20,7 +22,8 @@ resource "aws_security_group" "my_security_group" {
   description = "Inbound and outbound rules for the instance security group"
 }
 
-# Inbound rule - HTTP
+# Inbound rule - HTTP (web traffic, e.g. Nginx/Apache)
+
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.my_security_group.id
   cidr_ipv4         = "0.0.0.0/0"
@@ -29,29 +32,43 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   to_port           = 80
 }
 
+# Inbound rule - SSH (remote access)
+# Inbound rule - SSH (remote administration - restrict to your IP)
+
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.my_security_group.id
   cidr_ipv4         = "0.0.0.0/0"
+  # cidr_ipv4         = "YOUR_IP/32"  # Replace with your public IP (restrict to your IP)
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
 }
 
 
-# Outbound rule - allow all
+# Outbound rule - allow all traffic
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
   security_group_id = aws_security_group.my_security_group.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
-# EC2 instance
+# --------------------------------------------------
+# EC2 Instance Configuration
+# --------------------------------------------------
+
+# EC2 instance - single instance setup
+
+
 resource "aws_instance" "my_instance" {
   # count                  = 2   # 👈 ADD HERE
-  ami                    = "ami-07062e2a343acc423"
-  instance_type          = "t3.micro"
+  ami                    = "ami-07062e2a343acc423" # OS AMI ID
+
+  instance_type          = "t3.micro" # Instance Type
+
   key_name               = aws_key_pair.my_key_pair.key_name
-  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]  # VPC & Security Group
 
   root_block_device {
     volume_size = 10
